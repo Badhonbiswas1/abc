0ab80579d0c24a521574daf17702397cd3ce3a74
Badhonbiswas1 <badhonbiswas669@gmail.com>
2024-05-02 22:10:54 +0600

    add sub multi division

---
+++ addsubmulti.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,sum,sub,multi,division;
+ scanf("%d %d",&a,&b);
+ sum = a+b;
+ sub = a-b;
+ multi = a*b;
+ division = a/b;
+ printf(" Sum is: %d\n",sum);
+ printf(" sub is: %d\n",sub);
+ printf(" multi is: %d\n",multi);
+ printf(" division is: %d\n",division);
+ return 0;
+}
